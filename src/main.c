d1dcdf5318f60e490f97d72ff07f8079e06f8397
Dark-Gouloum <49821955+Dark-Gouloum@users.noreply.github.com>
2023-02-01 13:26:33 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,4 +1,36 @@
-#include <stdio.h>
+/**
+  * \file main.c
+  * \brief Le fichier source du produit final
+  * \author Clement FEFEU ; Victor FOUQUERAY ; Dylan GRAMMONT ; Erwan PECHON
+  * \version 0.1
+  * \date Ven. 27 Janv. 2023 14:27:35
+  *
+  * Le fichier source du jeu 'Le tombeau du désert ardent'.
+  * Aller voir le README.md pour plus de détaille.
+  *
+  */
 
-int main(){
-}
+// INCLUSION(S) DE(S) BIBLIOTHEQUE(S) NÉCÉSSAIRE(S)
+
+// CRÉATION(S) DE(S) CONSTANTE(S) NUMÉRIQUE(S)
+
+// CRÉATION(S) D(ES) ÉNUMÉRATION(S)
+
+// CRÉATION(S) D(ES) STRUCTURE(S) ET D(ES) UNIONS(S)
+
+// CRÉATION(S) DE(S) CONSTANTE(S) DE STRUCTURE(S)
+
+// CRÉATION(S) DE(S) FONCTION(S)
+
+[
+// PROGRAMME PRINCIPALE
+int main(){  /* Programme qui lance le tombeau du desert ardent */
+  // INITIALISATION DE(S) VARIABLE(S)
+	// INSTRUCTION(S)
+	// FIN DU PROGRAMME
+	return 0;
+} /* Programme qui lance le tombeau du desert ardent */
+// PROGRAMME PRINCIPALE
+]
+
+// #####-#####-#####-#####-##### FIN PROGRAMMATION #####-#####-#####-#####-##### //
