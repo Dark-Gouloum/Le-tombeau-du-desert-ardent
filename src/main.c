b76603262ef42e8f472d30ea65f20c34871d9b74
Erwan PÉCHON <Erwan.Pechon.Etu@univ-lemans.fr>
2023-02-08 01:10:11 +0100

    erwan 2023-02-08 mise à jour

--- src/main.c
+++ src/main.c
@@ -12,8 +12,11 @@
 
 // INCLUSION(S) DE(S) BIBLIOTHEQUE(S) NÉCÉSSAIRE(S)
 #include <SDL2/SDL.h>
-# include <SDL2/SDL_ttf.h>
-# include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 // CRÉATION(S) DE(S) CONSTANTE(S) NUMÉRIQUE(S)
 
 // CRÉATION(S) D(ES) ÉNUMÉRATION(S)
@@ -36,21 +39,40 @@ int main(){  /* Programme qui lance le tombeau du desert ardent */
 		// Création des variables dynamiques
 	SDL_Window *window = NULL;
 	SDL_Renderer *renderer = NULL;
+	SDL_Event event;
 	// INSTRUCTION(S)
 		// Création de la fenêtre
 	if( SDL_CreateWindowAndRenderer(500,500,SDL_WINDOW_SHOWN,&(window),&(renderer)) ){
 		printf("Erreur SDL_CreateWindowAndRenderer : %s", SDL_GetError());
 		goto Quit;
 	}
+	SDL_SetWindowTitle(window,"Le tombeau du désert ardant");
+		// Dessiner dans la fenêtre
+	sleep(1);
+	if( SDL_SetRenderDrawColor(renderer, 255,165,0,80) ){	printf("Erreur SDL_SetRenderDrawColor : %s", SDL_GetError());	goto Quit;	}
+	if( SDL_RenderClear(renderer) ){	printf("Erreur SDL lors de la création d'un dessin : %s", SDL_GetError());	goto Quit;	}
+	SDL_RenderPresent(renderer);
+	sleep(1);
+	if( SDL_SetRenderDrawColor(renderer, 0,165,255,80) ){	printf("Erreur SDL_SetRenderDrawColor : %s", SDL_GetError());	goto Quit;	}
+	if( SDL_RenderDrawLine(renderer, 0,0,1000,1000) ){	printf("Erreur SDL lors de la création d'un dessin : %s", SDL_GetError());	goto Quit;	}
+	SDL_RenderPresent(renderer);
+	sleep(1);
+		// Création de bouton
+	while(statut){
+		while( SDL_PollEvent(&event) ){
+			switch( event.type ){
+				case SDL_QUIT :	statut = 0;	break;
+			}
+		}
+	}
 	// FIN DU PROGRAMME
-	statut = 0;
 Quit:
 	if( renderer )
 		SDL_DestroyRenderer(renderer);
 	if( window )
 		SDL_DestroyWindow(window);
 	SDL_Quit();
-	return 0;
+	return statut;
 } /* Programme qui lance le tombeau du desert ardent */
 // PROGRAMME PRINCIPALE
 
